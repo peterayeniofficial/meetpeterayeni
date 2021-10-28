0699e0fdba8d35787ce6edd1e0aac3768cdd5daf
Peter Ayeni <ayenicology@gmail.com>
2021-10-28 23:21:28 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Peter Ayeni
+Software Engineer
+
+Peter is an award-winning Social Innovator, Software Engineer, and Mentor passionate about using design and technology for social good. He has worked as a Technical Mentor for Udacity React Nanodegree, and Frontend Developer at Previsico. He has an International Diploma in Computing from Informatics Singapore and a BBA in IT Security from SMC Switzerland.
+
+LinkedIn: https://www.linkedin.com/in/peterayeni/
+Twitter:  https://twitter.com/thepeterayeni
+Github:   https://github.com/peterayeniofficial
+
+`);
