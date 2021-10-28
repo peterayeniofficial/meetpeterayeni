9443de886b75435e7321381e6db5f0cbf749f01a
Peter Ayeni <ayenicology@gmail.com>
2021-10-29 00:57:17 +0100

    👌 IMPROVE: add color

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const welcome = require('cli-welcome');
 const pkgJSON = require('./package.json');
+const chalk = require('chalk');
+const log = console.log;
 
 welcome({
   title: pkgJSON.name,
@@ -13,14 +15,21 @@ welcome({
   clear: true,
 });
 
-console.log(`
-Peter Ayeni
+log(`
+${chalk.bold.blue('Peter Ayeni')}
 Software Engineer
 
-Peter is an award-winning Social Innovator, Software Engineer, and Mentor passionate about using design and technology for social good. He has worked as a Technical Mentor for Udacity React Nanodegree, and Frontend Developer at Previsico. He has an International Diploma in Computing from Informatics Singapore and a BBA in IT Security from SMC Switzerland.
-
-LinkedIn: https://www.linkedin.com/in/peterayeni/
-Twitter:  https://twitter.com/thepeterayeni
-Github:   https://github.com/peterayeniofficial
+${chalk.dim
+  .italic(`Peter is an award-winning Social Innovator, Software Engineer, and Mentor passionate about using design and technology for social good. He has worked as a Technical Mentor for Udacity React Nanodegree, and Frontend Developer at Previsico. He has an International Diploma in Computing from Informatics Singapore and a BBA in IT Security from SMC Switzerland.
+`)}
+${chalk.hex('#8d6cab').bold(`LinkedIn`)}: ${chalk.dim(
+  'https://www.linkedin.com/in/peterayeni/'
+)}
+${chalk.hex('#1da1f2').bold(`Twitter`)}:  ${chalk.dim(
+  'https://twitter.com/peterayeni'
+)}
+${chalk.hex('#6cc644').bold(`Github`)}:   ${chalk.dim(
+  'https://github.com/peterayeniofficial'
+)}
 
 `);
